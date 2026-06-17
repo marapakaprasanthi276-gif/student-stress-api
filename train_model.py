f09f009dfed0527434d3dac39ee3908283f66e90
Prasanthi Marapaka <marapakaprasanthi276@gmail.com>
2026-06-17 22:45:08 +0530

    updated trained model

--- train_model.py
+++ train_model.py
@@ -2,18 +2,22 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import joblib
 
-df = pd.read_csv("student_stress_dataset_1000_rows.csv")
+# Example dataset (you can replace with your real dataset)
+data = {
+    "study_hours": [1,2,3,4,5,6,7,8],
+    "sleep_hours": [8,7,6,5,4,3,2,1],
+    "screen_time": [2,3,4,5,6,7,8,9],
+    "stress_level": [0,0,0,1,1,1,1,1]
+}
 
-X = df.drop("Stress_Level", axis=1)
-y = df["Stress_Level"]
+df = pd.DataFrame(data)
 
-model = RandomForestClassifier(
-    n_estimators=100,
-    random_state=42
-)
+X = df[["study_hours", "sleep_hours", "screen_time"]]
+y = df["stress_level"]
 
+model = RandomForestClassifier()
 model.fit(X, y)
 
 joblib.dump(model, "stress_model.pkl")
 
-print("Model trained successfully!")
+print("Model created successfully!")
